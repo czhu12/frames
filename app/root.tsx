04f554790df5d60f36e745f73b5a33e972483dfe
Your Name <you@example.com>
2025-01-21 19:39:39 -0800

    fixed css

--- app/root.tsx
+++ app/root.tsx
@@ -7,9 +7,13 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: styles
+  },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
